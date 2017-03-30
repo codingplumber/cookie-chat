38c229ffc3f4bde74fa69da37a70308a86f45c41
codingplumber <jason.stickel@live.com>
2017-03-29 21:08:06 -0600

    finished cookie-chat

--- js/messageService.js
+++ js/messageService.js
@@ -4,7 +4,17 @@ angular.module('chatroom').service('messageService', function($http){
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
-
+  this.getMessages = function() {
+    var promise = $http({
+      method: 'GET',
+      url: 'https://practiceapi.devmounta.in/api/chats'
+    });
+    return promise.then(function(response) {
+      if (response.status === 200) {
+        return response.data;
+      };
+    });
+  };
 
 
 
@@ -12,8 +22,11 @@ angular.module('chatroom').service('messageService', function($http){
   //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
-
-
-
-
+  this.postMessage = function(message) {
+    $http({
+      method: 'POST',
+      url: 'https://practiceapi.devmounta.in/api/chats',
+      data: {message: message}
+    });
+  };
 });
